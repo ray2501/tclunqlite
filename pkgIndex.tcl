bd6c795d68a98e095081fb3022c9bb34f5e8e6a6
Danilo Chang <ray2501@gmail.com>
2016-03-04 20:38:43 +0800

    Set version to v0.2.7

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded unqlite 0.2.6 \
-    [list load [file join $dir libunqlite0.2.6.so] unqlite]
+package ifneeded unqlite 0.2.7 \
+    [list load [file join $dir libunqlite0.2.7.so] unqlite]
