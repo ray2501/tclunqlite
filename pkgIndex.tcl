de8233531088624c091d6b55b968b46c574806f3
Danilo Chang <ray2501@gmail.com>
2023-12-15 23:21:31 +0800

    Set version to v0.3.9

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded unqlite 0.3.8 \
-	    [list load [file join $dir libtcl9unqlite0.3.8.so] [string totitle unqlite]]
+    package ifneeded unqlite 0.3.9 \
+	    [list load [file join $dir libtcl9unqlite0.3.9.so] [string totitle unqlite]]
 } else {
-    package ifneeded unqlite 0.3.8 \
-	    [list load [file join $dir libunqlite0.3.8.so] [string totitle unqlite]]
+    package ifneeded unqlite 0.3.9 \
+	    [list load [file join $dir libunqlite0.3.9.so] [string totitle unqlite]]
 }
--- tclunqlite_SUSE.spec
+++ tclunqlite_SUSE.spec
@@ -4,7 +4,7 @@
 
 Name:          tclunqlite
 Summary:       Tcl interface for UnQLite
-Version:       0.3.8
+Version:       0.3.9
 Release:       2
 License:       BSD-2-Clause
 Group:         Development/Libraries/Tcl
