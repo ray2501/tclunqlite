3a7f989aad9ecc2be7168d66292e694ac855bb14
Danilo Chang <ray2501@gmail.com>
2023-12-15 23:18:17 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded unqlite 0.3.8 \
-    [list load [file join $dir libunqlite0.3.8.so] unqlite]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded unqlite 0.3.8 \
+	    [list load [file join $dir libtcl9unqlite0.3.8.so] [string totitle unqlite]]
+} else {
+    package ifneeded unqlite 0.3.8 \
+	    [list load [file join $dir libunqlite0.3.8.so] [string totitle unqlite]]
+}
